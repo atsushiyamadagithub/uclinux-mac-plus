fe37ecc0bea188ce4cf86281409c58f366ed8b41
atsushiyamadagithub <atsushi@ic-net.or.jp>
2026-06-03 22:25:12 +0900

    Add files via upload

---
+++ mame-images/make_boot_floppy_img.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+dd if=/dev/zero of=floppy.img bs=1024 count=800
+
+mke2fs -t ext2 -b 1024 -O none -I 128 floppy.img
+
+dd if=../bootloader/boot_block.bin of=floppy.img bs=1024 count=1 conv=notrunc
+
+mkdir -p mnt
+sudo mount -o loop floppy.img mnt
+
+sudo cp linux-2.0.x/linux mnt/kernel
+
+echo -n "root=/dev/sda rw init=/bin/sh" | sudo tee mnt/cmdline
+
+sync
+sudo umount mnt
